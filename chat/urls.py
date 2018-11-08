26a5b5cef0ee0fb271ddf717cdbf64e7aeddb1b0
Marini <nmarini@orangeusd.org>
2018-11-07 19:27:58 -0800

    updating urls

--- chat/urls.py
+++ chat/urls.py
@@ -4,7 +4,7 @@ import chat.views as views
 app_name = 'chat'
 
 urlpatterns = [
-	path('', views.home),
+	path('/', views.home),
 	path('chat/<str:room>/', views.chat, name='chat'),
-	path('login', views.login, name='login')
+	path('login/', views.login, name='login')
 ]
