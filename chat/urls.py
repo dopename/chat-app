429975cd404ff6b64dbcb511356fa80acf2fee2d
Marini <nmarini@orangeusd.org>
2018-11-07 19:41:28 -0800

    urls needs to work.. idk

--- chat/urls.py
+++ chat/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-import chat.views as views
+from chat import views
 
 app_name = 'chat'
 
 urlpatterns = [
 	path('', views.home),
 	path('chat/<str:room>/', views.chat, name='chat'),
-	path('login/', views.user_login, name='login'),
+	path('login/', views.user_login),
 ]
