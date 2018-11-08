7b5786788e0ffff60d5c14b7da911de97a4efdf5
Marini <nmarini@orangeusd.org>
2018-11-07 19:57:02 -0800

    attaching user to request..

--- chat/views.py
+++ chat/views.py
@@ -29,6 +29,6 @@ def user_login(request):
 		username = request.POST.get('username')
 		password = request.POST.get('password')
 
-		user = authenticate(username=username, password=password)
+		user = authenticate(request=request, username=username, password=password)
 
 		return render(request, 'chat/index.html')
