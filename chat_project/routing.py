5399f048dc014fa6de6e1a0b3fb95863c315a27b
Marini <nmarini@orangeusd.org>
2018-11-13 09:11:51 -0800

    adding session middleware

--- chat_project/routing.py
+++ chat_project/routing.py
@@ -2,6 +2,8 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter, ChannelNameRouter
 from django.conf.urls import url
 from channels.security.websocket import AllowedHostsOriginValidator, OriginValidator
+from channels.sessions import SessionMiddlewareStack
+
 
 from chat.consumers import ChatConsumer, GlobalConsumer
 from chat.testconsumers import GlobalWebsocket, ChatRoomConsumer
@@ -25,14 +27,16 @@ old_application = ProtocolTypeRouter({
 
 application = ProtocolTypeRouter({
 	'websocket': AllowedHostsOriginValidator(
-		AuthMiddlewareStack(
-			URLRouter(
-				[
-					url(r"^chat/(?P<room_name>[\w.@+-]+)/$", ChatRoomConsumer),
-					url(r"^logged_in/$", GlobalWebsocket),
-				]
-			)
+		SessionMiddlewareStack(
+			AuthMiddlewareStack(
+				URLRouter(
+					[
+						url(r"^chat/(?P<room_name>[\w.@+-]+)/$", ChatRoomConsumer),
+						url(r"^logged_in/$", GlobalWebsocket),
+					]
+				)
 
+			)
 		)
 	),
 	# "channel":ChannelNameRouter({
