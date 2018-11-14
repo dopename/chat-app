2f3c13f40a459ee010f73575357b20699d6ab45e
Marini <nmarini@orangeusd.org>
2018-11-14 11:26:51 -0800

    ugh more frontend changes - modularizing js ws functions

--- static/js/helpers.js
+++ static/js/helpers.js
@@ -17,3 +17,59 @@ function grabChatroom(select, display) {
 		document.getElementById(display).innerHTML = html
 	})
 }
+
+var openWebsocketConnection = (url_suffix) => {
+	var loc = window.location
+
+	var wsStart = "ws://"
+	if (loc.protocol == 'https:') {
+		wsStart = 'wss://'
+	}
+
+	var endpoint = wsStart + window.location.host + url_suffix;
+					
+	var socket = new WebSocket(endpoint)
+
+	return socket
+}
+
+var websocketManagment = (url_suffix, type) => {
+	var socket = openWebsocketConnection()
+
+	socket.onmessage = (e) => {
+		console.log("message", e);
+
+		var onlineUsers = document.getElementById('online-users');
+
+		var parsedData = JSON.parse(e.data);
+		var responseKeys = Object.keys(parsedData);
+		console.log(responseKeys)
+
+		if (responseKeys.indexOf('global_user_count_update') > -1) {
+			onlineUsers.innerHTML = parsedData.global_user_count_update.total_users;
+		}
+		if (responseKeys.indexOf('chatroom_user_count_update') > -1) {
+			var updateKey = parsedData.chatroom_user_count_update.room
+			var roomOnlineUsers = document.getElementById('badge_' + updateKey);
+			console.log(updateKey, parsedData.chatroom_user_count_update.user_count)
+						
+			roomOnlineUsers.innerHTML = parsedData.chatroom_user_count_update.user_count;
+		}
+		if (responseKeys.indexOf('user_logged_in') > -1) {
+			return null
+		}
+	}
+
+	socket.onopen = (e) => {
+		console.log("open", e)
+	}
+
+	socket.onerror = (e) => {
+		console.log("error", e)
+	}
+
+	socket.onclose = (e) => {
+		console.log("close", e)
+	}
+
+}
