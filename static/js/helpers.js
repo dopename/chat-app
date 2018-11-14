6652edaa445d7f7ec40062d58ee5299f34d8506b
Marini <nmarini@orangeusd.org>
2018-11-14 11:00:45 -0800

    frontend upgrade - putting js in helpers.js file

---
+++ static/js/helpers.js
@@ -0,0 +1,18 @@
+function scrollWindow(div) {
+	var windowHeightPx = (window.screen.height * 0.50).toString() + "px";
+	var chatBox = document.getElementById(div)
+	chatBox.style.height = windowHeightPx;
+	chatBox.scrollTop = chatBox.scrollHeight;
+}
+
+function grabChatroom(select, display) {
+	var url = '/chat/'
+	var selectBox = document.getElementById(select)
+	var selectedOption = selectBox.options[selectBox.selectedIndex].value
+
+	fetch(url + selectedOption)
+	.then(response => response.text())
+	.then(html => {
+		document.getElementById(display).innerHTML = html
+	})
+}
